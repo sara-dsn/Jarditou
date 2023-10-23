3f42c40d068d98972f34a57583650020bd550606
sara-dsn <142814795+sara-dsn@users.noreply.github.com>
2023-10-23 17:41:40 +0200

    Update jarditou.js

--- asset/js/jarditou.js
+++ asset/js/jarditou.js
@@ -11,36 +11,55 @@ function f_valid() {
 var envoi=true;
 
     var prenom = $("#prenom").val();
-if(prenom===""){
-    envoi=false;
-    alert("Entrez votre prénom s.v.p");
-}
+    if(prenom===""){
+        envoi=false;
+        $("#p").show();
+        e.preventDefault();
+    }
+    else{
+        $("#p").hide();
+        document.forms[0].submit();
+    };
 
 
    
     var cp = $("#cp").val();
     if(cp===""){
         envoi=false;
-        alert("Entrez votre code postale s.v.p");
+        $("#c").show();
+        e.preventDefault();
     }
+    else{
+        $("#c").hide();
+        document.forms[0].submit();
+    };
   
    
 
 
-    var mail = $("#mail").val();
-    if(mail===""){
+    var email = $("#mail").val();
+    if(email===""){
         envoi=false;
-        alert("Entrez votre email s.v.p");
+        $("#e").show();
+        e.preventDefault();
     }
+    else{
+        $("#e").hide();
+        document.forms[0].submit();
+    };
    
    
     
     var date = $("#date").val();
     if(date===""){
         envoi=false;
-        alert("Entrez votre date de naissance s.v.p");
+        $("#d").show();
+        e.preventDefault();
     }
-  
+    else{
+        $("#d").hide();
+        document.forms[0].submit();
+    };
    
 
     var question = $("#question").val();
@@ -76,98 +95,5 @@ $(document).ready(function(){
         f_valid();
     });
 });
-
-
-function f_valid() {
-var envoi=true;
-
-    var prenom = $("#prenom").val();
-if(prenom===""){
-    envoi=false;
-    $("#n").show();
-    e.preventDefault();
-}
-else{
-    $("#n").hide();
-    document.forms[0].submit();
 };
-
-
-   
-    var cp = $("#cp").val();
-    if(cp===""){
-        envoi=false;
-        $("#c").show();
-        e.preventDefault();
-    }
-    else{
-        $("#c").hide();
-        document.forms[0].submit();
-    };
-   
-
-
-    var mail = $("#mail").val();
-    if(mail===""){
-        envoi=false;
-        alert("Entrez votre email s.v.p");
-    }
-   
-   
-    
-    var date = $("#date").val();
-    if(date===""){
-        envoi=false;
-        alert("Entrez votre date de naissance s.v.p");
-    }
-  
-   
-
-    var question = $("#question").val();
-if(question===""){
-        envoi=false;
-        alert("Entrez votre question s.v.p");
-    }
-  
-   
-    var genre=$("input[name='genre']:checked").val();
-    if(genre===undefined){
-        envoi=false;
-        alert("Veuillez séléctionner votre genre s.v.p");
-    }
-   
-  
-
-    var sujet=$("input[name='sujet']:checked").val();
-    if(sujet===undefined){
-        envoi=false;
-        alert("Veuillez séléctionner un sujet s.v.p");
-    }
- 
-   
-    var ok= $("#ok").prop("checked");
-   if(!ok){
-        envoi=false;
-        $("#n").show();
-        e.preventDefault();
-    }
- 
-   
-    var nom =$("#nom").val();
-    if(nom===""){
-        envoie=false;
-        $("#n").show();
-        e.preventDefault();
-    }
-
-    if(envoi==true){
-        
-    }
- 
-    
-};
-
-};
-
 });
-
